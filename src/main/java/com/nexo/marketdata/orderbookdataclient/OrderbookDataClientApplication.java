c85f6d82df8feda8c350099239f8dd91b4863ce3
Stiliyan Vasilev <svasilev@vmware.com>
2022-12-26 15:15:33 +0200

    project setup

---
+++ src/main/java/com/nexo/marketdata/orderbookdataclient/OrderbookDataClientApplication.java
@@ -0,0 +1,13 @@
+package com.nexo.marketdata.orderbookdataclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderbookDataClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderbookDataClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nexo/marketdata/orderbookdataclient/OrderbookDataClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexo.marketdata.orderbookdataclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderbookDataClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
